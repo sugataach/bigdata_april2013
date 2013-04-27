2f5859b22c58972174163f0048c938a3cec05e40
Michael Law <mlaw135@gmail.com>
2013-04-27 15:45:24 -0400

    process.py and semantria_demo update

--- process.py
+++ process.py
@@ -1,5 +1,10 @@
 import urllib2
 import simplejson
+import requests
+import nltk
+import re
+from BeautifulSoup import BeautifulSoup
+
 
 user_input = "london"
 web_string = "http://content.guardianapis.com/search?q="+user_input+"&format=json"
@@ -14,3 +19,21 @@ count = 0
 for article in result["response"]["results"]:
     entry = str(article["webUrl"])
     ret_dict.append(entry)
+
+print ret_dict
+
+
+#for each url...
+url = 'http://www.guardian.co.uk/society/2013/apr/25/society-daily-email'
+response = requests.get(url)
+#parse html
+soup = BeautifulSoup(response.text)
+body_html = soup.findAll('div', attrs={'id':'article-body-blocks'})
+#strip trags
+body_text = nltk.clean_html(response.text)
+#remove excess spaces
+text = re.sub('\s+', ' ', body_text)
+#limit to 8000 chars
+text = [text[:8000]]
+print text
+
--- semantria_demo.py
+++ semantria_demo.py
@@ -1,88 +1,100 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
 import semantria
 import uuid
 import time
 
-print "Semantria service demo...", "\r\n"
+print("Semantria service demo ...", "\r\n")
 
 # the consumer key and secret
+#consumerKey = ""
+#consumerSecret = ""
 consumerKey = "c18c1d22-e189-4a92-b34d-dc1155964446"
 consumerSecret = "e7faedd6-70c2-487f-a43a-ad89aebf04e9"
 initialTexts = [
-    "Initial text for processing.", 
-    "Initial text for processing.", 
-    "Initial text for processing."
-    ]
+    "Lisa - there's 2 Skinny cow coupons available $5 skinny cow ice cream coupons on special k boxes and Printable FPC from facebook - a teeny tiny cup of ice cream. I printed off 2 (1 from my account and 1 from dh's). I couldn't find them instore and i'm not going to walmart before the 19th. Oh well sounds like i'm not missing much ...lol",
+    "In Lake Louise - a guided walk for the family with Great Divide Nature Tours  rent a canoe on Lake Louise or Moraine Lake  go for a hike to the Lake Agnes Tea House. In between Lake Louise and Banff - visit Marble Canyon or Johnson Canyon or both for family friendly short walks. In Banff  a picnic at Johnson Lake  rent a boat at Lake Minnewanka  hike up Tunnel Mountain  walk to the Bow Falls and the Fairmont Banff Springs Hotel  visit the Banff Park Museum. The \"must-do\" in Banff is a visit to the Banff Gondola and some time spent on Banff Avenue - think candy shops and ice cream.",
+    "On this day in 1786 - In New York City  commercial ice cream was manufactured for the first time."
+]
+
 
 def onRequest(sender, result):
-    print "\n", "REQUEST: ", result
+    print("\n", "REQUEST: ", result)
+
 
 def onResponse(sender, result):
-    print "\n", "RESPONSE: ", result
+    print("\n", "RESPONSE: ", result)
+
 
 def onError(sender, result):
-    print "\n", "ERROR: ", result
+    print("\n", "ERROR: ", result)
+
 
 def onDocsAutoResponse(sender, result):
-    print "\n", "AUTORESPONSE: ", len(result), result
+    print("\n", "AUTORESPONSE: ", len(result), result)
+
 
 def onCollsAutoResponse(sender, result):
-    print "\n", "AUTORESPONSE: ", len(result), result
+    print("\n", "AUTORESPONSE: ", len(result), result)
 
 # Creates JSON serializer instance
 serializer = semantria.JsonSerializer()
-
 # Initializes new session with the serializer object and the keys.
-session	= semantria.Session(consumerKey, consumerSecret, serializer)
+session = semantria.Session(consumerKey, consumerSecret, serializer, use_compression=True)
 
 # Initialize session callback handlers
-# session.Request += onRequest
-# session.Response += onResponse
+#session.Request += onRequest
+#session.Response += onResponse
 session.Error += onError
-#session.DocsAutoResponse +=	onDocsAutoResponse
-#session.CollsAutoResponse +=	onCollsAutoResponse
+#session.DocsAutoResponse += onDocsAutoResponse
+#session.CollsAutoResponse += onCollsAutoResponse
 
 for text in initialTexts:
     # Creates a sample document which need to be processed on Semantria
     # Unique document ID
     # Source text which need to be processed
-    doc = { "id":str(uuid.uuid1()).replace("-", ""), "text":text}
+    doc = {"id": str(uuid.uuid1()).replace("-", ""), "text": text}
     # Queues document for processing on Semantria service
     status = session.queueDocument(doc)
     # Check status from Semantria service
     if status == 202:
-        print "Document ", doc["id"], " queued successfully.", "\r\n"
+        print("\"", doc["id"], "\" document queued successfully.", "\r\n")
 
-#Count of the sample documents which need to be	processed on Semantria
-length	= len(initialTexts)
-results	= []
+# Count of the sample documents which need to be processed on Semantria
+length = len(initialTexts)
+results = []
 
-while (len(results) < length):
-    print "Please wait 5 sec for documents...", "\r\n"
-#As Semantria isn't real-time solution you need to wait some time before getting of the processed results In real application here can be implemented two separate jobs, one for queuing of source data another one for retreiving
-# Wait ten seconds while Semantria process queued document
-time.sleep(5)
-# Requests processed results from Semantria service
-status = session.getProcessedDocuments()
-# Check status from Semantria service
-if isinstance(status, list):
-    for object in status:
-        results.append(object)
-printlen(status), "	documents	received	successfully.", "\r\n"
+while len(results) < length:
+    print("Retrieving your processed results...", "\r\n")
+    # As Semantria isn't real-time solution you need to wait some time before getting of the processed results
+    # In real application here can be implemented two separate jobs, one for queuing of source data
+    # another one for retreiving
+    # Wait ten seconds while Semantria process queued document
+    time.sleep(2)	
+    # Requests processed results from Semantria service
+    status = session.getProcessedDocuments()
+    # Check status from Semantria service
+    if isinstance(status, list):
+        for object_ in status:
+            results.append(object_)
+
 for data in results:
     # Printing of document sentiment score
-    print "Document ", data["id"], " Sentiment score: ", data["sentiment_score"],"\r\n"
-    
-    # Printing of document themes
-    print "Document	themes:", "\r\n"
-    if data["doc_themes"]:
-        for theme in data["doc_themes"]:
-            print " ", theme["title"], " (sentiment: ",
-            theme["sentiment_score"], ")", "\r\n"
-    
-    # Printing of document entities
-    print "Entities:", "\r\n"
-    if data["entities"]:
-        for entity in data["entities"]:
-            print " ", entity["normalized_form"], " : ", entity["entity_type"], " (sentiment: ", entity["sentiment_score"], ")", "\r\n"
+    print("Document ", data["id"], " Sentiment score: ", data["sentiment_score"], "\r\n")
 
-i = raw_input("Enter to close app ...")
+    # Printing of document themes
+    if "themes" in data:
+        print("Document themes:", "\r\n")
+        for theme in data["themes"]:
+            print("	", theme["title"], " (sentiment: ", theme["sentiment_score"], ")", "\r\n")
+
+    # Printing of document entities
+    if "entities" in data:
+        print("Entities:", "\r\n")
+        for entity in data["entities"]:
+            print("\t",
+                  entity["title"], " : ", entity["entity_type"],
+                  " (sentiment: ", entity["sentiment_score"], ")", "\r\n"
+            )
+
+#i = raw_input("Enter to close app ...")
