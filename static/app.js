244d6218ad35402916389a0461da47d519b19497
Walter Miller <walter@waltermiller.ca>
2013-04-27 16:45:42 -0400

    Changes to front end

--- static/app.js
+++ static/app.js
@@ -1,5 +1,38 @@
+var places = [{'name': 'Camden'},
+ {'name': 'Greenwich'},
+ {'name': 'Hackney'},
+ {'name': 'Hammersmith and Fulham'},
+ {'name': 'Islington'},
+ {'name': 'Royal Borough of Kensington and Chelsea'},
+ {'name': 'Lambeth'},
+ {'name': 'Lewisham'},
+ {'name': 'Southwark'},
+ {'name': 'Tower Hamlets'},
+ {'name': 'Wandsworth'},
+ {'name': 'Westminster'},
+ {'name': 'Barking and Dagenham'},
+ {'name': 'Barnet'},
+ {'name': 'Bexley'},
+ {'name': 'Brent'},
+ {'name': 'Bromley'},
+ {'name': 'Croydon'},
+ {'name': 'Ealing'},
+ {'name': 'Enfield'},
+ {'name': 'Haringey'},
+ {'name': 'Harrow'},
+ {'name': 'Havering'},
+ {'name': 'Hillingdon'},
+ {'name': 'Hounslow'},
+ {'name': 'Kingston upon Thames'},
+ {'name': 'Merton'},
+ {'name': 'Newham'},
+ {'name': 'Redbridge'},
+ {'name': 'Richmond upon Thames'},
+ {'name': 'Sutton'},
+ {'name': 'Waltham Forest'}]
+
 function PlaceListCtrl($scope) {
-  $scope.places = [{name: 'Camden'}, {name: 'Grenwich'}, {name: 'Hackney'}];
+  $scope.places = places;
   $scope.current_place = null;
   $scope.show_spinner = false;
 
